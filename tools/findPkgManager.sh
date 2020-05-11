fbe357655f5638b10694ecc4228e89b3c3431eb1
Azkali <a.ffcc7@gmail.com>
2020-05-11 07:07:06 +0200

    fix: 🐛 URL generation, Adds CLI menu, remove spawn process

--- tools/findPkgManager.sh
+++ tools/findPkgManager.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Source : https://ilhicas.com/2018/08/08/bash-script-to-install-packages-multiple-os.html
 declare -A osInfo;
-osInfo[/etc/debian_version]="apt-get"
+osInfo[/etc/debian_version]="apt-get update -y && apt-get install -y"
 osInfo[/etc/alpine-release]="apk"
-osInfo[/etc/centos-release]="yum"
-osInfo[/etc/fedora-release]="dnf"
-osInfo[/etc/arch-release]="pacman"
+osInfo[/etc/centos-release]="yum update && yum install"
+osInfo[/etc/fedora-release]="dnf update && dnf install"
+osInfo[/etc/arch-release]="pacman -Syu"
 
 for f in ${!osInfo[@]}
 do
