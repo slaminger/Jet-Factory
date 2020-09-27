03c0c37b66e56e2d955c7cc8ab7c29e60cd8bab9
azkali <a.ffcc7@gmail.com>
2020-09-27 13:53:26 +0000

    Update: config for tumbleweed

--- configs/tegra210/files/swr-tumbleweed.sh
+++ configs/tegra210/files/swr-tumbleweed.sh
@@ -3,9 +3,11 @@
 echo "Installing XFCE, Nvidia drivers and switch config..."
 zypper -n ar --refresh -p 90 https://download.opensuse.org/repositories/home:/Azkali:/Switch-L4T/openSUSE_Tumbleweed/home:Azkali:Switch-L4T.repo
 zypper --gpg-auto-import-keys refresh
-zypper -n in firefox onboard langpacks-ja upower screen wpa_supplicant alsa-utils initial-setup-gui \
-	alsa-ucm alsa-plugins-pulseaudio pulseaudio pulseaudio-module-x11 pulseaudio-utils \
-	xorg-x11-xinit xorg-x11-drv-libinput xorg-x11-drv-wacom xorg-x11-drv-evdev bluez iw \
+zypper -n rm sddm xdm
+zypper -n in onboard upower screen wpa_supplicant alsa-utils \
+	patterns-base-x11_enhanced xorg-x11 xorg-x11-essentials \
+	alsa-ucm-conf alsa-plugins-pulse pulseaudio pulseaudio-module-x11 pulseaudio-utils \
+	xinit xf86-input-libinput xf86-input-wacom xf86-input-evdev bluez iw \
 	libnvmpi1_0_0 nvidia-l4t-init nvidia-l4t-multimedia nvidia-l4t-oem-config \
 	nvidia-l4t-3d-core nvidia-l4t-multimedia-utils \
 	nvidia-l4t-firmware nvidia-l4t-configs nvidia-l4t-tools \
@@ -13,7 +15,7 @@ zypper -n in firefox onboard langpacks-ja upower screen wpa_supplicant alsa-util
 	lightdm onboard lightdm-gtk-greeter
 # zypper -n in --oldpackage xorg-x11-server-1.19.6 xorg-x11-server-extra-1.19.6 xorg-x11-server-source-1.19.6
 zypper -n in switch-configs
-zypper -n clean all
+zypper -n clean -a
 systemctl enable r2p bluetooth NetworkManager lightdm
 echo "Done!"
 
